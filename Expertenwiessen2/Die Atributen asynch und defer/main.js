9d7375896c161852294fc104d2f295e44e696dfd
Mehdi-Fadaei <150168926+Mehdi-Fadaei@users.noreply.github.com>
2024-02-05 10:31:24 +0100

    Montag05.02.2024

---
+++ Expertenwiessen2/Die Atributen asynch und defer/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let meine_div = document.querySelector("#test");
+
+if (meine_div) {
+    console.log("Das <div>-element wurde gefunden!!");
+}else {
+    console.log("War wohl noch nicht da!");
+}
+
+
+
---
+++ Expertenwiessen2/bedingte ternaere Operatoren/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//Syntax  Bedingung ? Ausdruck_1 : Ausdruck_2 es gib wert zuruck braucht keien consol log 
+
+let meien_zahl = 10;
+
+console.log(meien_zahl > 10 ? "Gröser als 10" : "Kleienr als 10");
+
+
+
+
+
+
